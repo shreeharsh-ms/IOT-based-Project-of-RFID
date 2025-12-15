9e78c8cde9ecf7d463bea9ccbb5e4daaca3ad751
Shreeharsh <info.shreeharshshivpuje@gmail.com>
2025-12-15 14:08:17 +0530

    Hosting Started

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,9 +1,8 @@
 import sys
 import os
 
-# Ensure backend/app is importable
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from app import create_app
 
-app = create_app()  # <-- Vercel looks for THIS
+app = create_app()
