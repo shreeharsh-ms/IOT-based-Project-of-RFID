b507f88eb30340d053e8e4038e168e2c406c3bc1
Shreeharsh <info.shreeharshshivpuje@gmail.com>
2025-12-18 13:51:27 +0530

    m

--- backend/app/routes/admin_routes.py
+++ backend/app/routes/admin_routes.py
@@ -214,7 +214,7 @@ def send_sms_via_twilio(mobile_number, message):
         # 🔴 TEMPORARY HARDCODED VALUES
         account_sid = "AC6ce4fb7e878b36fdb126ef65f498caf2"
         auth_token = "5259f15df4da3c6f8c3abc0a8cb82b48"
-        twilio_number = "+13048026706"
+        twilio_number = "+14405811723"
 
         print("➡️ Account SID loaded:", account_sid[:6] + "****")
         print("➡️ Twilio number:", twilio_number)
